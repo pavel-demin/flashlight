6db379474950e47bc5025319a928c1a38c9c7cfa
Pavel Demin <pavel-demin@outlook.com>
2017-03-05 21:38:26 +0100

    add configuration bits

--- src/flashlight.c
+++ src/flashlight.c
@@ -1,5 +1,16 @@
 #include <xc.h>
 
+#pragma config FOSC = INTOSC // Oscillator Selection bits (INTOSC oscillator: CLKIN function disabled)
+#pragma config BOREN = OFF // Brown-out Reset Enable (Brown-out Reset disabled)
+#pragma config WDTE = OFF // Watchdog Timer Enable (WDT disabled)
+#pragma config PWRTE = OFF // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON // MCLR Pin Function Select bit (MCLR pin function is MCLR)
+#pragma config CP = OFF // Code Protection bit (Program memory code protection is disabled)
+#pragma config LVP = ON // Low-Voltage Programming Enable (Low-voltage programming enabled)
+#pragma config LPBOR = OFF // Brown-out Reset Selection bits (BOR disabled)
+#pragma config BORV = LO // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+#pragma config WRT = OFF // Flash Memory Self-Write Protection (Write protection off)
+
 #define _XTAL_FREQ 8000000
 
 persistent unsigned int key;
