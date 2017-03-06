30648841ab26087ee22c5d3b1ada7e830a162e8a
Pavel Demin <pavel-demin@outlook.com>
2017-03-07 00:41:07 +0100

    add two pins

--- pcb/flashlight.py
+++ pcb/flashlight.py
@@ -13,16 +13,16 @@ c3 = Part('parts', 'C', value = '1u', footprint = 'modules:0805')
 l1 = Part('parts', 'L', value = '2.2u', footprint = 'modules:XFL4020')
 
 vdd = Part('parts', 'PAD', ref = 'VDD', value = 'VDD', footprint = 'modules:PAD-1.0mm')
-vss = Part('parts', 'PAD', ref = 'VSS', value = 'VSS', footprint = 'modules:PAD-1.0mm')
-clk = Part('parts', 'PAD', ref = 'CLK', value = 'PCLK', footprint = 'modules:PAD-1.0mm')
-dat = Part('parts', 'PAD', ref = 'DAT', value = 'PDAT', footprint = 'modules:PAD-1.0mm')
-clr = Part('parts', 'PAD', ref = 'CLR', value = 'MCLR', footprint = 'modules:PAD-1.0mm')
+clk = Part('parts', 'PAD', ref = 'PCLK', value = 'PCLK', footprint = 'modules:PAD-1.0mm')
+dat = Part('parts', 'PAD', ref = 'PDAT', value = 'PDAT', footprint = 'modules:PAD-1.0mm')
+clr = Part('parts', 'PAD', ref = 'MCLR', value = 'MCLR', footprint = 'modules:PAD-1.0mm')
 
 ledp = Part('parts', 'PAD', ref = '+', value = '+LED', footprint = 'modules:PAD-1.5mm')
 ledn = Part('parts', 'PAD', ref = '-', value = 'LED-', footprint = 'modules:PAD-1.5mm')
 
 vias_supply = 3 * Part('parts', 'VIA', footprint = 'modules:VIA-0.6mm', dest = TEMPLATE)
-vias_gnd = 9 * Part('parts', 'VIA', footprint = 'modules:VIA-0.6mm', dest = TEMPLATE)
+vias_gnd = 6 * Part('parts', 'VIA', footprint = 'modules:VIA-0.6mm', dest = TEMPLATE)
+pins_gnd = 2 * Part('parts', 'PIN', footprint = 'modules:PIN-1.0mm', dest = TEMPLATE)
 
 supply = Net('SUPPLY')
 gnd = Net('GND')
@@ -30,8 +30,8 @@ nets = 7 * Net()
 
 u1[:] += NC
 u2[:] += NC
-supply += c1[1], l1[1], u1[5, 6, 8]
-gnd += c1[2], c2[2], c3[2], u1[4, 9, 11], r1[2], u2[1, 7, 9], vss[1]
+supply += c1[1], l1[1], u1[5, 6, 8], u2[3]
+gnd += c1[2], c2[2], c3[2], u1[4, 9, 11], r1[2], u2[7, 9]
 nets[0] += l1[2], u1[3]
 nets[1] += u1[2], c2[1], ledp[1]
 nets[2] += u1[1], c3[1], u2[2], r2[2], vdd[1]
@@ -42,5 +42,6 @@ nets[6] += u2[8], clr[1]
 
 for via in vias_supply: supply += via[1]
 for via in vias_gnd: gnd += via[1]
+for pin in pins_gnd: gnd += pin[1]
 
 generate_netlist()
