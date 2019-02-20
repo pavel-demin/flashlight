debb71c49c8f32ca7be1239c8b72e5ec8c6927e2
Pavel Demin <pavel-demin@outlook.com>
2019-02-20 16:07:51 +0100

    remove net codes from flashlight.tcl

--- pcb/flashlight.tcl
+++ pcb/flashlight.tcl
@@ -2,8 +2,8 @@ proc part {ref value footprint} {
   puts "    (comp (ref $ref) (value $value) (footprint modules:$footprint))"
 }
 
-proc wire {code name args} {
-  puts "    (net (code $code) (name $name)"
+proc wire {name args} {
+  puts "    (net (name $name)"
   foreach element $args {
     set list [split $element /]
     puts "      (node (ref [lindex $list 0]) (pin [lindex $list 1]))"
@@ -44,15 +44,15 @@ for {set i 1} {$i <= 9} {incr i} {
 puts "  )"
 puts "  (nets"
 
-wire 0 GND C1/2 C2/2 C3/2 IC1/11 IC1/4 IC1/9 IC2/7 IC2/9 PIN1/1 PIN2/1 R1/2 R2/1 VIA4/1 VIA5/1 VIA6/1 VIA7/1 VIA8/1 VIA9/1
-wire 1 N1 IC1/3 L1/2
-wire 2 N2 +/1 C2/1 IC1/2
-wire 3 N3 C3/1 IC1/1 IC2/2 R2/2 VDD/1
-wire 4 N4 IC1/7 IC2/4 PCLK/1
-wire 5 N5 -/1 IC1/10 R1/1
-wire 6 N6 IC2/5 PDAT/1
-wire 7 N7 IC2/8 MCLR/1
-wire 8 SUPPLY C1/1 IC1/5 IC1/6 IC1/8 IC2/3 L1/1 VIA1/1 VIA2/1 VIA3/1
+wire GND C1/2 C2/2 C3/2 IC1/11 IC1/4 IC1/9 IC2/7 IC2/9 PIN1/1 PIN2/1 R1/2 R2/1 VIA4/1 VIA5/1 VIA6/1 VIA7/1 VIA8/1 VIA9/1
+wire N1 IC1/3 L1/2
+wire N2 +/1 C2/1 IC1/2
+wire N3 C3/1 IC1/1 IC2/2 R2/2 VDD/1
+wire N4 IC1/7 IC2/4 PCLK/1
+wire N5 -/1 IC1/10 R1/1
+wire N6 IC2/5 PDAT/1
+wire N7 IC2/8 MCLR/1
+wire SUPPLY C1/1 IC1/5 IC1/6 IC1/8 IC2/3 L1/1 VIA1/1 VIA2/1 VIA3/1
 
 puts "  )"
 puts ")"
